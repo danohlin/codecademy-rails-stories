598efc01d2737e4cfd052f56990be5a97f94cf51
codecademy <ccuser@codecademy.com>
2016-03-03 21:27:18 -0500

    added contact page functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   
   get 'about' => 'pages#about'
   
+  resources :signups
+  
+  get 'thanks' => 'pages#thanks'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
